077fac404b1b611214df700ac30da6678d81b12b
The0CO <theo.guichard@lecole-ldlc.com>
2025-09-10 14:54:00 +0200

    maj

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('header');
+    const logo = document.querySelector('.logo');
+  if (logo) {
+    logo.style.display = 'flex';
+    logo.style.alignItems = 'center';
+  }
   const toggle = document.querySelector('.menu-toggle');
   const dropdownLinks = document.querySelectorAll('.dropdown > a');
 
